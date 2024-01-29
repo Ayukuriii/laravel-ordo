6887f4474d2e1f1ccb95e7850dee3753ad48d15c
Ekadian Haris <ekadianharis@gmail.com>
2024-01-29 11:36:26 +0700

    materi routing ✅

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/perkalian/{angka?}', function ($angka = null) {
+    $res = $angka * 2;
+
+    return $res;
+});
